575f88931877da55d2432be75b5981c0174dda3b
rmichela <deltahat@gmail.com>
2011-10-29 16:18:55 -0400

    Added OfflinePlayer metadata support
    OfflinePlayer and Player now use the same metadata store, allowing
    metadata about players to be manipulated independent of their online
    status.

--- src/com/ryanmichela/metadatademo/MetadataDemo.java
+++ src/com/ryanmichela/metadatademo/MetadataDemo.java
@@ -9,7 +9,7 @@ import org.bukkit.event.block.BlockListener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerBucketFillEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -20,9 +20,11 @@ public class MetadataDemo extends JavaPlugin
 {
     public void onEnable() {
         getServer().getLogger().info("Loading metadata demo...");
+        getServer().getOfflinePlayer("deltahat").setMetadata("fromOffline", new FixedMetadataValue(this, true));
         getServer().getPluginManager().registerEvent(Event.Type.BLOCK_PLACE, new BlockPlaceListener(this), Event.Priority.Normal, this);
         getServer().getPluginManager().registerEvent(Event.Type.PLAYER_INTERACT, new PlayerStickListener(), Event.Priority.Normal, this);
-        getServer().getPluginManager().registerEvent(Event.Type.PLAYER_BUCKET_FILL, new PlayerBucketListener(this), Event.Priority.Normal, this);
+        getServer().getPluginManager().registerEvent(Event.Type.PLAYER_BUCKET_FILL, new PlayerListener(this), Event.Priority.Normal, this);
+        getServer().getPluginManager().registerEvent(Event.Type.PLAYER_JOIN, new PlayerListener(this), Event.Priority.Normal, this);
     }
 
     public void onDisable() {
@@ -40,11 +42,11 @@ public class MetadataDemo extends JavaPlugin
         public void onBlockPlace(BlockPlaceEvent event) {
             Block b = event.getBlockPlaced();
             Player p = event.getPlayer();
-            b.addMetadata("placedBy", new FixedMetadataValue(plugin, p.getName()));
+            b.setMetadata("placedBy", new FixedMetadataValue(plugin, p.getName()));
         }
     }
 
-    private class PlayerStickListener extends PlayerListener {
+    private class PlayerStickListener extends org.bukkit.event.player.PlayerListener {
         @Override
         public void onPlayerInteract(PlayerInteractEvent event) {
             if(event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getMaterial() == Material.STICK) {
@@ -57,10 +59,10 @@ public class MetadataDemo extends JavaPlugin
         }
     }
 
-    private class PlayerBucketListener extends PlayerListener {
+    private class PlayerListener extends org.bukkit.event.player.PlayerListener {
         private Plugin plugin;
 
-        public PlayerBucketListener(Plugin plugin) {
+        public PlayerListener(Plugin plugin) {
             this.plugin = plugin;
         }
 
@@ -70,7 +72,15 @@ public class MetadataDemo extends JavaPlugin
             if(p.hasMetadata("bucketFilled")) {
                 p.sendMessage("You have filled a bucket before.");
             } else {
-                p.addMetadata("bucketFilled", new FixedMetadataValue(plugin, true));
+                p.setMetadata("bucketFilled", new FixedMetadataValue(plugin, true));
+            }
+        }
+
+        @Override
+        public void onPlayerJoin(PlayerJoinEvent event) {
+            Player p = event.getPlayer();
+            if(p.hasMetadata("fromOffline")) {
+                p.sendMessage("You have metadata from when you were offline.");
             }
         }
     }
